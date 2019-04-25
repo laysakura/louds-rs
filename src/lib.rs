421fdb2d2e7b2e8266fc829101c4e70aacf755f1
Sho Nakatani <lay.sakura@gmail.com>
2019-04-25 12:50:58 +0900

    Library settings

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+//! High performance LOUDS (Level-Order Unary Degree Sequence) library.
+//!
+//! [Master API Docs](https://laysakura.github.io/louds-rs/louds_rs/)
+//! |
+//! [Released API Docs](https://docs.rs/crate/louds-rs)
+//! |
+//! [Benchmark Results](https://laysakura.github.io/louds-rs/criterion/report/)
+//! |
+//! [Changelog](https://github.com/laysakura/louds-rs/blob/master/CHANGELOG.md)
+//!
+//! [![Build Status](https://travis-ci.com/laysakura/louds-rs.svg?branch=master)](https://travis-ci.com/laysakura/louds-rs)
+//! [![Crates.io](https://img.shields.io/crates/v/louds-rs.svg)](https://crates.io/crates/louds-rs)
+//! [![Minimum rustc version](https://img.shields.io/badge/rustc-1.33+-lightgray.svg)](https://github.com/laysakura/louds-rs#rust-version-supports)
+//! [![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/laysakura/louds-rs/blob/master/LICENSE-MIT)
+//! [![License: Apache 2.0](https://img.shields.io/badge/license-Apache_2.0-blue.svg)](https://github.com/laysakura/louds-rs/blob/master/LICENSE-APACHE)
+//!
+//! # Quickstart
+//!
+//! To use louds-rs, add the following to your `Cargo.toml` file:
+//!
+//! ```toml
+//! [dependencies]
+//! louds-rs = "0.1"
+//! ```
+//!
+//! ## Usage Overview
+//! (TBD)
+//!
+//!
+//! # Features
+//! (TBD)
+//!
+//! ## Complexity
+//! (TBD)
