49ee0e4c5d9666eea0d87888d121d15424f6f577
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-04-01 17:17:12 +0200

    Added derives for memdbg/memsize

--- src/louds.rs
+++ src/louds.rs
@@ -6,6 +6,9 @@ use fid_rs::Fid;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+#[cfg(feature = "mem_dbg")]
+use mem_dbg::{MemDbg, MemSize};
+
 /// LOUDS (Level-Order Unary Degree Sequence).
 ///
 /// This class can handle tree structure of virtually **arbitrary number of nodes**.
@@ -14,18 +17,21 @@ use serde::{Deserialize, Serialize};
 /// It should be enough for almost all usecases since a binary data of length of _2^63_ consumes _2^20 = 1,048,576_ TB (terabytes), which is hard to handle by state-of-the-art computer architecture.
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "mem_dbg", derive(MemDbg, MemSize))]
 pub struct Louds {
     lbs: Fid,
 }
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "mem_dbg", derive(MemDbg, MemSize))]
 #[repr(transparent)]
 /// Node number of [Louds](struct.Louds.html) tree.
 pub struct LoudsNodeNum(pub u64);
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "mem_dbg", derive(MemDbg, MemSize))]
 #[repr(transparent)]
 /// Index of [Louds](struct.Louds.html) tree.
 pub struct LoudsIndex(pub u64);
